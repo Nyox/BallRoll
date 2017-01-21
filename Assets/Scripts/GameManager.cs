befec0d011c91efbacdbef729b5630a21a16c742
Nyox <nicholas.dannunzio@gmail.com>
2017-01-21 18:25:58 +0100

    update
    last update

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -26,18 +26,18 @@ public class GameManager : MonoBehaviour {
 		Camera.main.rect = new Rect(0f, 0f, 1f, 1f);
 		mazeInstance = Instantiate(mazePrefab) as Maze;
 		yield return StartCoroutine(mazeInstance.Generate());
-		playerInstance = Instantiate(playerPrefab) as Player;
-		playerInstance.SetLocation(mazeInstance.GetCell(mazeInstance.RandomCoordinates));
+		/*playerInstance = Instantiate(playerPrefab) as Player;
+		playerInstance.SetLocation(mazeInstance.GetCell(mazeInstance.RandomCoordinates));*/
 		Camera.main.clearFlags = CameraClearFlags.Depth;
-		Camera.main.rect = new Rect(0f, 0f, 0.5f, 0.5f);
+		//Camera.main.rect = new Rect(0f, 0f, 0.5f, 0.5f);
 	}
 
 	private void RestartGame () {
 		StopAllCoroutines();
 		Destroy(mazeInstance.gameObject);
-		if (playerInstance != null) {
+		/*if (playerInstance != null) {
 			Destroy(playerInstance.gameObject);
-		}
+		}*/
 		StartCoroutine(BeginGame());
 	}
 }
